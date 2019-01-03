4bcdb24d8df8a9ecb71a90556e0873e66f95f12c
monaiim <m.o.dehaoui@gmail.com>
2019-01-03 23:31:57 +0100

    Initial commit

---
+++ uma-transporter/src/main/java/com/univers/architecture/transporter/dao/ITaskExecutionRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.univers.architecture.transporter.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.univers.architecture.transporter.model.TaskExecution;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @author sabir
+ *
+ */
+public interface ITaskExecutionRepository extends PagingAndSortingRepository<TaskExecution, String> {
+
+}
